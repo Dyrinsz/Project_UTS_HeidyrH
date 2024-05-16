ae9ed340587d39d914565fac4cf0805121cbfca4
Dyrinsz <122091216+Dyrinsz@users.noreply.github.com>
2024-05-16 23:28:07 +0700

    Project UTS_Muhammmad Heidyr Hadiningrat_202232035

---
+++ docs/assets/scripts.js
@@ -0,0 +1,37 @@
+//JavaScript of responsive navigation menu
+
+const menuBtn = document.querySelector(".menu-btn");
+const navegation = document.querySelector(".navegation");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navegation.classList.toggle("active");
+});
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
